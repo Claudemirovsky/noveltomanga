42a2ffb521e0d1baca03c0496983296f241b5788
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-09-11 12:05:51 -0300

    Prevent STUB Error on Extensions-Inspector.jar

--- lib/src/main/kotlin/org/claudemirovsky/noveltomanga/Theme.kt
+++ lib/src/main/kotlin/org/claudemirovsky/noveltomanga/Theme.kt
@@ -1,23 +1,25 @@
 package org.claudemirovsky.noveltomanga
 
-import android.graphics.Color
-
 enum class DefaultThemes : Theme {
     BLACK { // Almost...
-        override val backgroundColor = Color.rgb(12, 12, 12)
-        override val fontColor = Color.rgb(239, 239, 239)
+        override val backgroundColor = rgbaColor(12, 12, 12)
+        override val fontColor = rgbaColor(239, 239, 239)
     },
     DARK {
-        override val backgroundColor = Color.rgb(33, 33, 33)
-        override val fontColor = Color.rgb(245, 245, 245)
+        override val backgroundColor = rgbaColor(33, 33, 33)
+        override val fontColor = rgbaColor(245, 245, 245)
     },
     LIGHT {
-        override val backgroundColor = Color.rgb(244, 244, 244)
-        override val fontColor = Color.rgb(34, 34, 34)
+        override val backgroundColor = rgbaColor(244, 244, 244)
+        override val fontColor = rgbaColor(34, 34, 34)
     }
 }
 
 interface Theme {
     val backgroundColor: Int
     val fontColor: Int
+}
+
+fun Theme.rgbaColor(R: Int, G: Int, B: Int, A: Int = 255): Int {
+    return (A and 0xff) shl 24 or (R and 0xff) shl 16 or (G and 0xff) shl 8 or (B and 0xff)
 }
